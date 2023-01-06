5fe61f3c6731f498df53ede884127d288a2f8844
Hosana <hosanabarceloscandido@gmail.com>
2023-01-05 23:49:20 -0300

    feat: add metodos

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,22 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { Notification } from '../../../../application/entities/notification';
-import { NotificationsRepository } from '../../../../application/repositories/notifications-repository';
+import { Notification } from '@application/entities/notification';
+import { NotificationsRepository } from '@application/repositories/notifications-repository';
+import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper';
 import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
     constructor(private prismaService: PrismaService) {}
-
+    
+    findById(notificationId: string): Promise<Notification> {
+        throw new Error('Method not implemented.');
+    }
+    
+    findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+        throw new Error('Method not implemented.');
+    }
+    
+    countManyRecipientId(recipientId: string): Promise<number> {
+        throw new Error('Method not implemented.');
+    }
+    
     async create(notification: Notification): Promise<void> {
+        const raw = PrismaNotificationMapper.toPrisma(notification)
         await this.prismaService.notification.create({
-            data: {
-                id: notification.id,
-                category: notification.category,
-                content: notification.content.value,
-                recipientId: notification.recipientId,
-                readAt: notification.readAt,
-                createdAt: notification.createdAt,
-            }
+            data: raw,
         });
+    }
+    
+    save(notification: Notification): Promise<void> {
+        throw new Error('Method not implemented.');
     }
 }
